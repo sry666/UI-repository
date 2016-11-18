615cfab7be164ffa947886f1163a0e697b81af0e
sry666 <894883177@qq.com>
2016-11-18 17:12:04 +0800

    ani_design

---
+++ ani design demo/src/index.js
@@ -0,0 +1,30 @@
+import './index.html';
+import './index.css';
+import dva from 'dva';
+
+// 1. Initialize
+//const app = dva();
+const app = dva({
+	initialState: {
+     products: [
+       { name: 'dva', id: 1 },
+       { name: 'antd', id: 2 },
+     ],
+   },	
+})
+ 
+ 
+// 2. Plugins
+app.use({});
+
+// 3. Model
+app.model(require('./models/products'));
+
+// 4. Router
+app.router(require('./router'));
+
+// 5. Start
+app.start('#root');
+
+
+ 
